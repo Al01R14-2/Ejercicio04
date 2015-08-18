a8572ea14689adccda15d4e945272349ec0920a6
Teutle <saul_nai3@hotmail.com>
2015-08-18 11:36:16 -0500

    Segunda
    subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,23 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	
-}); 
+//document.addEventListener("deviceready",function(){
+	$('#btn_sonido').click(function(){
+		alert ("click a btn-sonido")
+		if ($('#btn_sonido').hasClass('ui-icon-audio'))
+		{
+			//Apagar Audio
+			$('#btn_sonido').removeClass('ui-icon-audio');
+			$('#btn_sonido').addClass('ui-icon-forbidden');
+			alert('se apago el sonido');
+		}
+		
+			else
+			{
+				 $('#btn_sonido').removeClass('ui-icon-forbidden');
+		         $('#btn_sonido').addClass('ui-icon-audio');
+		    alert('se prendio el audio');
+		}
+	});
+//}); 
 });
 
